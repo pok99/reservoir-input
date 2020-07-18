1ea0060d546b39d5d129206b953197f0118c2391
lzil <liang.zhou.18@ucl.ac.uk>
2020-07-18 01:15:11 +0100

    use identity to look at J, instead of tanh

--- first-pass/check_chaos.py
+++ first-pass/check_chaos.py
@@ -9,7 +9,7 @@ import random
 from network import Network, Reservoir
 from utils import Bunch
 
-b = Bunch(N=20, D=1, res_init_params={'std':1.5}, reservoir_noise=0.1, reservoir_burn_steps=200)
+b = Bunch(N=20, D=1, res_init_params={'std':1.5}, reservoir_noise=.1, reservoir_burn_steps=200)
 
 net = Network(b)
 
@@ -22,10 +22,12 @@ all_dists = []
 n_steps = 300
 n_net_reps = 12
 n_reps = 15
+activation = lambda y: y
 
 for rep in range(n_net_reps):
 
     net = Network(b)
+    net.reservoir.activation = activation
     init_x = np.random.normal(0, 1, (1, N))
     net.reservoir.reset(res_state=init_x)
     #x1 = net.reservoir.x.numpy().reshape(-1)
@@ -37,7 +39,7 @@ for rep in range(n_net_reps):
     dists = []
 
     for i in range(n_reps):
-        new_x = init_x + np.random.normal(0, .1, (1, N))
+        new_x = init_x + np.random.normal(0, 1, (1, N))
         net.reservoir.reset(res_state=new_x)
         #x2 = net.reservoir.x.detach().numpy().reshape(-1)
         xss = np.zeros((n_steps, N))
@@ -57,24 +59,4 @@ for i in range(n_net_reps):
     for j in range(n_reps):
         plt.plot(all_dists[i][j])
 
-    # plt.xlabel('steps since burn in')
-    # plt.ylabel('distance')
 plt.show()
-
-
-    #print('i', x1, '\nn', x2)
-
-#     corr = np.dot(x1/np.linalg.norm(x1), x2/np.linalg.norm(x2))
-#     dist = np.linalg.norm(x1 - x2)
-#     #print(corr)
-#     corrs.append(corr)
-#     dists.append(dist)
-
-# plt.subplot(2, 1, 1)
-# plt.hist(corrs, bins=20, range=[-1,1])
-
-# plt.subplot(2, 1, 2)
-# plt.hist(dists, bins=30, alpha=0.5)
-
-# plt.tight_layout()
-# plt.show()
