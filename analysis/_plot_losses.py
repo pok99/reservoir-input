2dcf6f08892fec370575c7e8a81bfeb957186a26
lzil <liang.zhou.18@ucl.ac.uk>
2020-11-12 17:26:35 +0000

    loss plot

--- analysis/_plot_losses.py
+++ analysis/_plot_losses.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+
+
 import pdb
 import os
 import sys
@@ -10,6 +12,8 @@ import subprocess
 sys.path.append('../')
 
 from utils import get_config
+
+# plt.rcParams["font.family"] = "space mono"
 
 run_id = '3539456'
 
@@ -55,55 +59,76 @@ rnoises = dt['rnoise'].unique()
 mnoises = dt['mnoise'].unique()
 rseeds = dt['rseed'].unique()
 
-color_scale = ['coral', 'chartreuse', 'skyblue']
+color_scale = ['coral', 'cornflowerblue', 'skyblue']
 
-fig, axes = plt.subplots(nrows=len(mnoises), ncols=len(rseeds), sharex=True, sharey=True, figsize=(14,10))
-fig.text(0.07, 0.5, 'loss', va='center', rotation='vertical')
-fig.text(0.5, 0.04, 'D', ha='center')
+# fig.text(0.07, 0.5, 'loss', va='center', rotation='vertical')
+# fig.text(0.5, 0.04, 'D', ha='center')
 
 # dt = dt[(dt.dset == 'datasets/rsg-sohn.pkl')]
+
+# pdb.set_trace()
 # dt = dt[(dt.rnoise == 0.01)]
-for i, mnoise in enumerate(mnoises):
-    for j, rseed in enumerate(rseeds):
-        subset = dt[(dt.mnoise == mnoise) & (dt.rseed == rseed)]
+dt = dt[(dt.mnoise == 4)]
 
-        ax = axes[i, j]
-        ax.set_xticklabels([0] + list(Ds))
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-        # ax.tick_params()
-        # ax.xaxis.set_ticks_position('bottom')
-        # ax.tick_params(which='major', width=1.00, length=4)
-        if i == 0:
-            ax.set_title('seed = ' + str(rseed))
-        if j == 0:
-            ax.set_ylabel('noise = ' + str(mnoise))
-        
-        # subset_all = subset[subset.]
-        train_all = subset[subset.tparts == 'all']
-        ax.scatter(train_all.D_map, train_all.loss, s=8, alpha=.7, c=color_scale[0], label='train all')
-        means = []
-        for D in Ds:
-            means.append(np.mean(train_all[train_all.D == D]['loss']))
-        ax.plot(range(len(Ds)), means, c=color_scale[0], ms=20)
-        train_lim = subset[subset.tparts == 'W_f-W_ro']
-        ax.scatter(train_lim.D_map, train_lim.loss, s=8, alpha=.7, c=color_scale[1], label='train part')
-        means = []
-        for D in Ds:
-            means.append(np.mean(train_lim[train_lim.D == D]['loss']))
-        ax.plot(range(len(Ds)), means, c=color_scale[1], ms=50)
+plt.figure(figsize=(5,4))
+ax = plt.gca()
+ax.set_xticklabels([0] + list(Ds), fontsize=9)
+plt.yticks(fontsize=9)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+# plt.tick_params()
+# plt.xaxis.set_ticks_position('bottom')
+# plt.tick_params(which='major', width=1.00, length=4)
+# if i == 0:
+#     plt.set_title('seed = ' + str(rseed))
+# if j == 0:
+#     plt.set_ylabel('noise = ' + str(mnoise))
 
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        # ax.spines['bottom'].set_visible(False)
-        # ax.spines['left'].set_visible(False)
-        # ax.axhline(y=0, color='dimgray', alpha = 1)
-        # ax.axvline(x=-.5, color='dimgray', alpha = 1)
-        # ax.tick_params(axis='both', color='white')
-        ax.grid(None)
-        ax.grid(True, which='major', axis='y', lw=1, color='lightgray', alpha=0.4)
-        ax.set_xlim([-.5, len(Ds) - .5])
-        ax.set_ylim([0, 100])
+train_all = dt[dt.tparts == 'all']
+train_lim = dt[dt.tparts == 'W_f-W_ro']
+plt.scatter(train_all.D_map, train_all.loss, s=8, alpha=.3, c=color_scale[0])
+plt.scatter(train_lim.D_map, train_lim.loss, s=8, alpha=.3, c=color_scale[1])
+q_all = []
+q_lim = []
+for D in Ds:
+    try:
+        # q = np.quantile(train_all[train_all.D == D]['loss'], [.25, .5, .75])
+        # q = [q[1], q[1] - q[0], q[2] - q[1]]
+        mn = np.mean(train_all[train_all.D == D]['loss'])
+        sd = np.std(train_all[train_all.D == D]['loss'])
+        q = [mn, sd, sd]
+        q_all.append(q)
+    except IndexError:
+        q_all.append([0,0,0])
+    try:
+        # q = np.quantile(train_lim[train_lim.D == D]['loss'], [.25, .5, .75])
+        # q = [q[1], q[1] - q[0], q[2] - q[1]]
+        mn = np.mean(train_lim[train_lim.D == D]['loss'])
+        sd = np.std(train_lim[train_lim.D == D]['loss'])
+        q = [mn, sd, sd]
+        q_lim.append(q)
+    except IndexError:
+        q_lim.append([0,0,0])
+q_all = list(zip(*q_all))
+q_lim = list(zip(*q_lim))
+plt.errorbar(range(len(Ds)), q_all[0], yerr = [q_all[1], q_all[2]], c=color_scale[0], ms=20, elinewidth=2, lw=3, label='trained RNN')
+plt.errorbar(range(len(Ds)), q_lim[0], yerr = [q_lim[1], q_lim[2]], c=color_scale[1], ms=20, elinewidth=2, lw=3, label='fixed RNN')
 
+# plt.plot(range(len(Ds)), means, c=color_scale[1], ms=50)
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+# plt.spines['bottom'].set_visible(False)
+# plt.spines['left'].set_visible(False)
+# plt.axhline(y=0, color='dimgray', alpha = 1)
+# plt.axvline(x=-.5, color='dimgray', alpha = 1)
+# plt.tick_params(axis='both', color='white')
+plt.grid(None)
+plt.grid(True, which='major', axis='y', lw=1, color='lightgray', alpha=0.4)
+plt.xlim([-.5, len(Ds) - .5])
+plt.ylim([0, 70])
+
+plt.xlabel('D')
+plt.ylabel('loss')
 
 plt.legend()
 plt.show()
